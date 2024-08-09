176eea82ca95006524e59bcdc665dbd00356d306
Avinasha <tejassriavinasha@gmail.com>
2024-08-09 23:27:17 +0530

    modified prisma schema

--- prisma/db/prisma.js
+++ prisma/db/prisma.js
@@ -1,13 +1,34 @@
 
 import { PrismaClient } from '@prisma/client';
+import PrismaAccelerate from 'prisma-accelerate'
 
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+    prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: process.env.DATABASE_URL,
+          },
+        },
+        log: ['query'],
+        __internal: {
+          hooks: PrismaAccelerate({}) // Initialize Prisma Accelerate
+        }
+      })
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient();
+        global.prisma = new new PrismaClient({
+            datasources: {
+              db: {
+                url: process.env.DATABASE_URL,
+              },
+            },
+            log: ['query'],
+            __internal: {
+              hooks: PrismaAccelerate({}) // Initialize Prisma Accelerate
+            }
+          })
     }
     prisma = global.prisma;
 }

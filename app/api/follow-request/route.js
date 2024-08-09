f236358c526d7063283f462f5d5e9ab35eeee54b
Avinasha <tejassriavinasha@gmail.com>
2024-08-09 23:06:41 +0530

    modified prisma client

--- app/api/follow-request/route.js
+++ app/api/follow-request/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from '@/prisma/db/prisma';
+// const prisma = new PrismaClient();
 
 export async function POST(request) {
   const body = await request.json();
--- app/api/follow-status/route.js
+++ app/api/follow-status/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/prisma/db/prisma';
+// const prisma = new PrismaClient();
 
 export async function GET(request) {
   try {
